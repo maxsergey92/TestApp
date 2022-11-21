a54f0ad10ee934db1c566787c3bdce5e8d4816e8
Sergey <sergeyruem@mail.ru>
2022-11-22 02:57:28 +0300

    init

---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using TestApp.MyUnits;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tr = new TankUnit();
+            tr.Fire(1);
+            Console.WriteLine(tr.GetStatuses());
+            Console.ReadKey();
+        }
+    }
+}
